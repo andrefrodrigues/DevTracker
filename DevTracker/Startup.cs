4595f4368d2925419d8095c87da102717146d986
André Rodrigues <andrefrodrigues21@gmail.com>
2017-01-16 18:29:54 +0000

    Add project files.

---
+++ DevTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevTracker.Startup))]
+namespace DevTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
