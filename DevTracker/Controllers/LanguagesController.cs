5c25b09adfc35c572c925a6282133604a30822ae
André Rodrigues <andrefrodrigues21@gmail.com>
2017-01-26 10:45:06 +0000

    Added a new feature

---
+++ DevTracker/Controllers/LanguagesController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DevTracker.Models;
+
+namespace DevTracker.Controllers
+{
+    public class LanguagesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Languages
+        public ActionResult Index()
+        {
+            return View(db.Languages.ToList());
+        }
+
+        // GET: Languages/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Language language = db.Languages.Find(id);
+            if (language == null)
+            {
+                return HttpNotFound();
+            }
+            return View(language);
+        }
+
+        // GET: Languages/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Languages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "name")] Language language)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Languages.Add(language);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(language);
+        }
+        // POST: Languages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Language language = db.Languages.Find(id);
+            db.Languages.Remove(language);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
